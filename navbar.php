c944b69e76a60c43fc2caf00c130e6be1eef800e
yassineZerdani-code <yassinezerdani.gd@gmail.com>
2020-09-13 14:51:34 +0100

    login mesg

---
+++ navbar.php
@@ -0,0 +1,69 @@
+<!-- Header -->
+<header class="main-header " id="header">
+        <nav class="navbar navbar-static-top navbar-expand-lg">
+
+          <!-- Sidebar toggle button -->
+          <button id="sidebar-toggler" class="sidebar-toggle">
+            <span class="sr-only">Toggle navigation</span>
+          </button>
+          <!-- search form -->
+          <div class="search-form d-none d-lg-inline-block">
+            <div class="input-group">
+            </div>
+            <div id="search-results-container">
+              <ul id="search-results"></ul>
+            </div>
+          </div>
+
+          <div class="navbar-right ">
+            <ul class="nav navbar-nav">
+              <li class="dropdown notifications-menu">
+                <button class="dropdown-toggle" data-toggle="dropdown">
+                  <i class="mdi mdi-bell-outline"></i>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-right">
+                  <li class="dropdown-header">You have 5 notifications</li>
+                  <li>
+                    <a href="#">
+                      <i class="mdi mdi-account-plus"></i> New user registered
+                      <span class=" font-size-12 d-inline-block float-right"><i class="mdi mdi-clock-outline"></i> 10
+                        AM</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="mdi mdi-account-remove"></i> User deleted
+                      <span class=" font-size-12 d-inline-block float-right"><i class="mdi mdi-clock-outline"></i> 07
+                        AM</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="mdi mdi-chart-areaspline"></i> Sales report is ready
+                      <span class=" font-size-12 d-inline-block float-right"><i class="mdi mdi-clock-outline"></i> 12
+                        PM</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="mdi mdi-account-supervisor"></i> New client
+                      <span class=" font-size-12 d-inline-block float-right"><i class="mdi mdi-clock-outline"></i> 10
+                        AM</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="mdi mdi-server-network-off"></i> Server overloaded
+                      <span class=" font-size-12 d-inline-block float-right"><i class="mdi mdi-clock-outline"></i> 05
+                        AM</span>
+                    </a>
+                  </li>
+                  <li class="dropdown-footer">
+                    <a class="text-center" href="#"> View All </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
