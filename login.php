299db93d4287df1d41de51a4323d931680e9cfcb
yassineZerdani-code <yassinezerdani.gd@gmail.com>
2020-09-13 14:33:33 +0100

    login mesg

--- login.php
+++ login.php
@@ -38,7 +38,7 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/my-login.css">
 	 <!-- FAVICON -->
-	 <link href="coffee.png" rel="shortcut icon" />
+	 <link href="wah.png" rel="shortcut icon" />
 </head>
 
 <body class="my-login-page">
@@ -47,7 +47,7 @@
 			<div class="row justify-content-md-center h-100">
 				<div class="card-wrapper">
 					<div class="brand">
-						<img src="coffee.png" alt="logo">
+						<img src="wah.png" alt="logo">
 					</div>
 					<div class="card fat">
 						<div class="card-body">
